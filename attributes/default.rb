f0ea99694e8ee8c9931aace280720bca21bf75e6
Jordan Yaker <jordan.e.yaker@gmail.com>
2014-08-08 10:37:57 -0400

    Modified recipes to work . . . hopefully.

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default[:bundler][:bundler_deployment] = false
+default[:bundler][:bundler_without_groups] = []
+default[:bundler][:bundle_command] = "bundle"
+default[:bundler][:bundle_options] = nil
+default[:bundler][:path] = nil
+default[:bundler][:user] = nil
+default[:bundler][:environment] = 'development'
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,6 +1,22 @@
-rbenv_script "run bundle install in app directory" do
+Chef::Log.info "Running bundle install"
+common_groups = node[:bundler][:bundler_without_groups]
+common_groups = common_groups.join(' ')
+
+bundler_deployment = node[:bundler][:bundler_deployment]
+if bundler_deployment
+  # Check for a Gemfile.lock
+  bundler_deployment = ::File.exists?(::File.join(new_resource.release_path, "Gemfile.lock"))
+end
+
+bundle_options = node[:bundler][:bundle_options]
+
+command = "#{node[:bundler][:bundle_command]} install"
+command += " --without #{common_groups}" if common_groups
+command += " --deployment" if bundler_deployment
+command += " #{bundle_options}" if bundle_options
+
+execute command do
+  cwd node[:bundler][:path]
   user node[:bundler][:user]
-  group node[:bundler][:group]    
-  cwd File.join(node[:bundler][:apps_path], node[:bundler][:app])
-  code %{bundle install}
+  environment node[:bundler][:environment]
 end
