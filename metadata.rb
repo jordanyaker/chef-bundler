b53b1000052a630f5581d756754fac3b65daf05c
Jordan Yaker <jordan.e.yaker@gmail.com>
2014-08-08 10:46:58 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jordan.e.yaker@gmail.com'
 license          'All rights reserved'
 description      'Installs the bundler gem and provides recipes to bundle actions'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.0'
+version          '0.8.1'
 
 recipe "bundler",          "Installs the bundler gem on the system."
 recipe "bundler::install", "Installs the gems specified by the Gemfile or Gemfile.lock files for an application."
